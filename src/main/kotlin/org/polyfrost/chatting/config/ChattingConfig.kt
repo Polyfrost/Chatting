70192294f34d0784885263cd6d813b86a1d4cc68
ev chang <wyvestbusiness@gmail.com>
2024-06-22 04:18:56 +0700

    i forgor

--- src/main/kotlin/org/polyfrost/chatting/config/ChattingConfig.kt
+++ src/main/kotlin/org/polyfrost/chatting/config/ChattingConfig.kt
@@ -345,6 +345,7 @@ object ChattingConfig : Config(
             if (OldPatcherConfig.transparentChat) {
                 if (OldPatcherConfig.transparentChatOnlyWhenClosed) {
                     chatWindow.setBackgroundColor(chatWindow.getBackgroundColor().also { it.alpha = 0 })
+                    chatWindow.differentOpacity = true
                     chatWindow.openOpacity = 255
                 } else {
                     chatWindow.setBackground(false)
