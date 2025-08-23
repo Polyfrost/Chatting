68faaae19496a8d91b34950ebccf7e512bd4dc8b
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-08-23 13:57:20 -0400

    Revert "Add loop and loopDelay commands for testing unsafe chat messages"
    This reverts commit de91cb3b4b452de47b6e62db4f7e7101a676cea2.

--- src/main/kotlin/org/polyfrost/chatting/command/ChattingCommand.kt
+++ src/main/kotlin/org/polyfrost/chatting/command/ChattingCommand.kt
@@ -1,36 +1,14 @@
 package org.polyfrost.chatting.command
 
-import cc.polyfrost.oneconfig.libs.universal.UChat
 import cc.polyfrost.oneconfig.utils.commands.annotations.Command
 import cc.polyfrost.oneconfig.utils.commands.annotations.Main
-import cc.polyfrost.oneconfig.utils.commands.annotations.SubCommand
 import org.polyfrost.chatting.Chatting
 import org.polyfrost.chatting.config.ChattingConfig
-import kotlin.concurrent.thread
 
 @Command(value = Chatting.ID, description = "Access the " + Chatting.NAME + " GUI.")
 class ChattingCommand {
     @Main
     fun main() {
         ChattingConfig.openGui()
-    }
-
-    @SubCommand
-    fun loop(amt: Int) {
-        thread {
-            for (i in 1..amt) {
-                UChat.chat(i)
-            }
-        }
-    }
-
-    @SubCommand
-    fun loopDelay(amt: Int, delay: Int) {
-        thread {
-            for (i in 1..amt) {
-                UChat.chat(i)
-                Thread.sleep(delay.toLong())
-            }
-        }
     }
 }
