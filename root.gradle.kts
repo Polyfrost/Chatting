f0962ff0f49076d792562e86c097df6db6624971
ImToggle <98242902+ImToggle@users.noreply.github.com>
2025-10-03 23:22:13 +0800

    removed unused mapping

--- root.gradle.kts
+++ root.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 preprocess {
     "1.21.5-fabric"(1_21_05, "yarn") {
         "1.21.4-fabric"(1_21_04, "yarn") {
-            "1.21.1-fabric"(1_21_01, "yarn") {//, file("versions/mappings/1.12.2-fabric+1.12.2-forge.txt")) {
+            "1.21.1-fabric"(1_21_01, "yarn") {
                 "1.16.5-fabric"(1_16_05, "yarn", file("versions/mappings/1.21.1-fabric+1.16.5-fabric.txt")) {
                     "1.12.2-fabric"(1_12_02, "yarn", file("versions/mappings/1.16.5-fabric+1.12.2-fabric.txt")) {
                         "1.12.2-forge"(1_12_02, "srg") {
