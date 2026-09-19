a80a87ddf4844e65be12d4318f0ccd6e23095e15
Prajol David <prajoldavid2@gmail.com>
2026-09-19 17:50:27 +0530

    chore(ornithe): remove retained forge target

--- src/dummy/java/club/sk1er/patcher/config/OldPatcherConfig.java
+++
@@ -1,7 +0,0 @@
-package club.sk1er.patcher.config;
-
-public class OldPatcherConfig {
-    public static boolean transparentChat; // Chatting
-    public static boolean transparentChatOnlyWhenClosed; // Chatting
-    public static boolean transparentChatInputField; // Chatting
-}
--- src/main/java/org/polyfrost/chatting/hook/ChatHook.java
+++
@@ -1,8 +0,0 @@
-package org.polyfrost.chatting.hook;
-
-import net.minecraft.client.gui.ChatLine;
-
-public class ChatHook {
-    public static ChatLine currentLine = null;
-    public static boolean lineVisible = false;
-}
